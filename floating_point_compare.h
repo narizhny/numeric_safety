693995c918ccfbfe72f3ecc8912b367fa7e332dc
narizhny <narizhny@inbox.ru>
2017-04-13 17:43:17 +0300

    Performance Troubleshooting :)

--- floating_point_compare.h
+++ floating_point_compare.h
@@ -3,10 +3,19 @@
 
 #include <math.h>
 
+namespace detail
+{
+template<class T, int precission>
+struct delimiter
+{
+    static constexpr T value = std::pow<T>(static_cast<T>(0.1), precission);
+};
+}
+
 template<int precission = 8, class Left, class Right>
 constexpr bool floating_point_compare(const Left &left, const Right &right)
 {
-    return std::abs(left - right) < (std::pow(0.1, precission) / 2);
+    return std::abs(left - right) < detail::delimiter<Left, precission>::value;
 }
 
 #endif // FLOATING_POINT_COMPARE_H
